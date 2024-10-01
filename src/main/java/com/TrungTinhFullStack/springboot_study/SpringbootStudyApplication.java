da83e4eca915023fbc5adc4cf1cdef713c0ccea9
nguyentrungtinh <trungtinhn300@gmail.com>
2024-10-01 08:47:37 +0700

    Create project for study springboot

---
+++ src/main/java/com/TrungTinhFullStack/springboot_study/SpringbootStudyApplication.java
@@ -0,0 +1,13 @@
+package com.TrungTinhFullStack.springboot_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootStudyApplication.class, args);
+	}
+
+}
